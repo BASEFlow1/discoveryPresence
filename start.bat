61320c28d527936fa0683390e856176a996a7d0c
BASEFlow1 <67925673+BASEFlow1@users.noreply.github.com>
2021-09-28 21:17:55 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,7 +1,7 @@
 @echo off
 :START
 tasklist /fi "ImageName eq Discord.exe" /fo csv 2>NUL | find /I "discord.exe">NUL
-if "%ERRORLEVEL%"=="0" python discoveryPresence.py "A:\Spiele\Freelancer\Discovery Freelancer 4.91.0"
+if "%ERRORLEVEL%"=="0" python discoveryPresence.py "path/to/disco/here"
 timeout /T 5 /NOBREAK >NUL
 goto START
 pause >nul
